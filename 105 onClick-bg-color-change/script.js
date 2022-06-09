e5f2289286dc001e3e5221adfc94300411ad60b6
MYSTYX <cool.abhi@outlook.com>
2022-06-09 23:54:05 +0530

    Initial Commit

---
+++ 105 onClick-bg-color-change/script.js
@@ -0,0 +1,11 @@
+const clickButton = document.querySelectorAll("body");
+const box = document.querySelectorAll(".box");
+clickButton.addEventListener("click", show);
+console.log(typeof(clickButton))
+function show() {
+  // box.forEach((item) =>{
+  //     item.classList.add('.click')
+  // })
+ 
+  console.log("red");
+}
